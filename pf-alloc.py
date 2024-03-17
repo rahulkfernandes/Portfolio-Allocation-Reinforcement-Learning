d0bedc25dc66c2f0ec8d5867060657d43b2459eb
rahulkfernandes <rahulkfernandes@gmail.com>
2024-03-17 16:35:53 +0530

    first commit

---
+++ pf-alloc.py
@@ -0,0 +1,359 @@
+# Load libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+
+from keras.layers import Input, Dense, Flatten, Dropout
+from keras.models import Model
+
+import random
+import matplotlib.pylab as plt
+
+import statsmodels.api as sm
+from statsmodels import regression
+
+DATASET = 'data/crypto_portfolio.csv'
+
+class CryptoEnvironment:
+    def __init__(self, prices = DATASET, capital=1e6):
+        self.prices = prices
+        self.capital = capital
+        self.data = self.load_data()
+
+    def load_data(self):
+        data = pd.read_csv(self.prices)
+        try:
+            data.index = data['Date']
+            data = data.drop(columns = ['Date'])
+        except:
+            data.index = data['date']
+            data = data.drop(columns = ['date'])
+        return data
+    
+    def preprocess_state(self, state):
+        return state
+    
+    def get_state(self, t, lookback, is_cov_matrix=True, is_raw_time_series=False):
+        assert lookback <= t
+
+        decision_making_state = self.data.iloc[t - lookback:t]
+        decision_making_state = decision_making_state.pct_change().dropna()
+
+        if is_cov_matrix:
+            x = decision_making_state.cov()
+            return x
+        else:
+            if is_raw_time_series:
+                decision_making_state = self.data.iloc[t - lookback:t]
+            return self.preprocess_state(decision_making_state)
+        
+    def get_reward(self, action, action_t, reward_t, alpha=0.01):
+        def local_portfolio(returns, weights):
+            weights = np.array(weights)
+            rets = returns.mean()
+            covs = returns.cov()
+            P_ret = np.sum(rets * weights)
+            P_vol = np.sqrt(np.dot(weights.T, np.dot(covs, weights)))
+            P_sharpe = P_ret/P_vol
+            return np.array([P_ret, P_vol, P_sharpe])
+        
+        data_period = self.data[action_t:reward_t]
+        weights = action
+        returns = data_period.pct_change().dropna()
+
+        sharpe = local_portfolio(returns, weights)[-1]
+        sharpe = np.array([sharpe] * len(self.data.columns))
+        ret = (data_period.values[-1] - data_period.values[0]) / data_period.values[0]
+        return np.dot(returns, weights), ret
+    
+class Agent:
+
+    def __init__(
+                    self, 
+                    portfolio_size,
+                    is_eval = False, 
+                    allow_short = True,
+                ):
+        
+        self.portfolio_size = portfolio_size
+        self.allow_short = allow_short
+        self.input_shape = (portfolio_size, portfolio_size, )
+        self.action_size = 3 # sit, buy, sell
+        
+        self.memory4replay = []
+        self.is_eval = is_eval
+
+        self.alpha = 0.5
+        self.gamma = 0.95
+        self.epsilon = 1
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.99
+        
+        self.model = self._model()
+
+    def _model(self):
+        
+        inputs = Input(shape=self.input_shape)        
+        x = Flatten()(inputs)
+        x = Dense(100, activation='elu')(x)
+        x = Dropout(0.5)(x)
+        x = Dense(50, activation='elu')(x)
+        x = Dropout(0.5)(x)
+        
+        predictions = []
+        for i in range(self.portfolio_size):
+            asset_dense = Dense(self.action_size, activation='linear')(x)   
+            predictions.append(asset_dense)
+        
+        model = Model(inputs=inputs, outputs=predictions)
+        model.compile(optimizer='adam', loss='mse')
+        return model
+
+    def nn_pred_to_weights(self, pred, allow_short = False):
+
+        weights = np.zeros(len(pred))
+        raw_weights = np.argmax(pred, axis=-1)
+
+        saved_min = None
+        
+        for e, r in enumerate(raw_weights):
+            if r == 0: # sit
+                weights[e] = 0
+            elif r == 1: # buy
+                weights[e] = np.abs(pred[e][0][r])
+            else:
+                weights[e] = -np.abs(pred[e][0][r])
+        #sum of absolute values in short is allowed
+        if not allow_short:
+            weights += np.abs(np.min(weights))
+            saved_min = np.abs(np.min(weights))
+            saved_sum = np.sum(weights)
+        else:
+            saved_sum = np.sum(np.abs(weights))
+            
+        weights /= saved_sum
+        return weights, saved_min, saved_sum
+    #return the action based on the state, uses the NN function 
+    def act(self, state):
+        
+        if not self.is_eval and random.random() <= self.epsilon:
+            w = np.random.normal(0, 1, size = (self.portfolio_size, ))  
+            
+            saved_min = None
+            
+            if not self.allow_short:
+                w += np.abs(np.min(w))
+                saved_min = np.abs(np.min(w))
+                
+            saved_sum = np.sum(w)
+            w /= saved_sum
+            return w , saved_min, saved_sum
+
+        pred = self.model.predict(np.expand_dims(state.values, 0))
+        return self.nn_pred_to_weights(pred, self.allow_short)
+
+    def expReplay(self, batch_size):
+
+        def weights_to_nn_preds_with_reward(action_weights, 
+                                            reward, 
+                                            Q_star = np.zeros((self.portfolio_size, self.action_size))): 
+            
+            Q = np.zeros((self.portfolio_size, self.action_size))           
+            for i in range(self.portfolio_size):
+                if action_weights[i] == 0:
+                    Q[i][0] = reward[i] + self.gamma * np.max(Q_star[i][0])
+                elif action_weights[i] > 0:
+                    Q[i][1] = reward[i] + self.gamma * np.max(Q_star[i][1])
+                else:
+                    Q[i][2] = reward[i] + self.gamma * np.max(Q_star[i][2])            
+            return Q  
+        
+        def restore_Q_from_weights_and_stats(action):            
+            action_weights, action_min, action_sum = action[0], action[1], action[2]
+            action_weights = action_weights * action_sum          
+            if action_min != None:
+                action_weights = action_weights - action_min   
+            return action_weights
+        
+        for (s, s_, action, reward, done) in self.memory4replay:
+            
+            action_weights = restore_Q_from_weights_and_stats(action) 
+            #Reward =reward if not in the terminal state. 
+            Q_learned_value = weights_to_nn_preds_with_reward(action_weights, reward)
+            s, s_ = s.values, s_.values    
+
+            if not done:
+                # reward + gamma * Q^*(s_, a_)
+                Q_star = self.model.predict(np.expand_dims(s_, 0))
+                Q_learned_value = weights_to_nn_preds_with_reward(action_weights, reward, np.squeeze(Q_star))  
+
+            Q_learned_value = [xi.reshape(1, -1) for xi in Q_learned_value]
+            Q_current_value = self.model.predict(np.expand_dims(s, 0))
+            Q = [np.add(a * (1-self.alpha), q * self.alpha) for a, q in zip(Q_current_value, Q_learned_value)]
+            
+            # update current Q function with new optimal value
+            self.model.fit(np.expand_dims(s, 0), Q, epochs=1, verbose=0)            
+        
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+
+if __name__ == "__main__":
+    N_ASSETS = 15 #53
+    agent = Agent(N_ASSETS)
+    env = CryptoEnvironment()
+    window_size = 180
+    episode_count = 50
+    batch_size = 32
+    rebalance_period = 90 #every 90 days weight change
+
+    for e in range(episode_count):
+    
+        agent.is_eval = False
+        data_length = len(env.data)
+        
+        returns_history = []
+        returns_history_equal = []
+        
+        rewards_history = []
+        equal_rewards = []
+        
+        actions_to_show = []
+        
+        print("Episode " + str(e) + "/" + str(episode_count), 'epsilon', agent.epsilon)
+
+        s = env.get_state(np.random.randint(window_size+1, data_length-window_size-1), window_size)
+        total_profit = 0 
+
+        for t in range(window_size, data_length, rebalance_period):
+            date1 = t-rebalance_period
+            #correlation from 90-180 days 
+            s_ = env.get_state(t, window_size)
+            action = agent.act(s_)
+            
+            actions_to_show.append(action[0])
+            
+            weighted_returns, reward = env.get_reward(action[0], date1, t)
+            weighted_returns_equal, reward_equal = env.get_reward(
+                np.ones(agent.portfolio_size) / agent.portfolio_size, date1, t)
+
+            rewards_history.append(reward)
+            equal_rewards.append(reward_equal)
+            returns_history.extend(weighted_returns)
+            returns_history_equal.extend(weighted_returns_equal)
+
+            done = True if t == data_length else False
+            agent.memory4replay.append((s, s_, action, reward, done))
+            
+            if len(agent.memory4replay) >= batch_size:
+                agent.expReplay(batch_size)
+                agent.memory4replay = []
+                
+            s = s_
+
+        rl_result = np.array(returns_history).cumsum()
+        equal_result = np.array(returns_history_equal).cumsum()
+
+        plt.figure(figsize = (12, 2))
+        plt.plot(rl_result, color = 'black', ls = '-')
+        plt.plot(equal_result, color = 'grey', ls = '--')
+        plt.show()
+        
+        plt.figure(figsize = (12, 2))
+        for a in actions_to_show:    
+            plt.bar(np.arange(N_ASSETS), a, color = 'grey', alpha = 0.25)
+            plt.xticks(np.arange(N_ASSETS), env.data.columns, rotation='vertical')
+        plt.show()
+
+
+    agent.is_eval = True
+
+    actions_equal, actions_rl = [], []
+    result_equal, result_rl = [], []
+
+    for t in range(window_size, len(env.data), rebalance_period):
+
+        date1 = t-rebalance_period
+        s_ = env.get_state(t, window_size)
+        action = agent.act(s_)
+
+        weighted_returns, reward = env.get_reward(action[0], date1, t)
+        weighted_returns_equal, reward_equal = env.get_reward(
+            np.ones(agent.portfolio_size) / agent.portfolio_size, date1, t)
+
+        result_equal.append(weighted_returns_equal.tolist())
+        actions_equal.append(np.ones(agent.portfolio_size) / agent.portfolio_size)
+        
+        result_rl.append(weighted_returns.tolist())
+        actions_rl.append(action[0])
+
+        result_equal_vis = [item for sublist in result_equal for item in sublist]
+        result_rl_vis = [item for sublist in result_rl for item in sublist]
+
+        plt.figure()
+        plt.plot(np.array(result_equal_vis).cumsum(), label = 'Benchmark', color = 'grey',ls = '--')
+        plt.plot(np.array(result_rl_vis).cumsum(), label = 'Deep RL portfolio', color = 'black',ls = '-')
+        plt.show()
+
+        #Plotting the data
+    import matplotlib
+    current_cmap = matplotlib.cm.get_cmap()
+    current_cmap.set_bad(color='red')
+
+    N = len(np.array([item for sublist in result_equal for item in sublist]).cumsum()) 
+
+    for i in range(0, len(actions_rl)):
+        current_range = np.arange(0, N)
+        current_ts = np.zeros(N)
+        current_ts2 = np.zeros(N)
+
+        ts_benchmark = np.array([item for sublist in result_equal[:i+1] for item in sublist]).cumsum()
+        ts_target = np.array([item for sublist in result_rl[:i+1] for item in sublist]).cumsum()
+
+        t = len(ts_benchmark)
+        current_ts[:t] = ts_benchmark
+        current_ts2[:t] = ts_target
+
+        current_ts[current_ts == 0] = ts_benchmark[-1]
+        current_ts2[current_ts2 == 0] = ts_target[-1]
+
+        plt.figure(figsize = (12, 10))
+
+        plt.subplot(2, 1, 1)
+        plt.bar(np.arange(N_ASSETS), actions_rl[i], color = 'grey')
+        plt.xticks(np.arange(N_ASSETS), env.data.columns, rotation='vertical')
+
+        plt.subplot(2, 1, 2)
+        plt.colormaps = current_cmap
+        plt.plot(current_range[:t], current_ts[:t], color = 'black', label = 'Benchmark')
+        plt.plot(current_range[:t], current_ts2[:t], color = 'red', label = 'Deep RL portfolio')
+        plt.plot(current_range[t:], current_ts[t:], ls = '--', lw = .1, color = 'black')
+        plt.autoscale(False)
+        plt.ylim([-1, 1])
+        plt.legend()
+
+
+    def sharpe(R):
+        r = np.diff(R)
+        sr = r.mean()/r.std() * np.sqrt(252)
+        return sr
+
+    def print_stats(result, benchmark):
+
+        sharpe_ratio = sharpe(np.array(result).cumsum())
+        returns = np.mean(np.array(result))
+        volatility = np.std(np.array(result))
+        
+        X = benchmark
+        y = result
+        x = sm.add_constant(X)
+        model = regression.linear_model.OLS(y, x).fit()    
+        alpha = model.params[0]
+        beta = model.params[1]
+        
+        return np.round(np.array([returns, volatility, sharpe_ratio, alpha, beta]), 4).tolist()
+    
+    
+    print('EQUAL', print_stats(result_equal_vis, result_equal_vis))
+    print('RL AGENT', print_stats(result_rl_vis, result_equal_vis))
